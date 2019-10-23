1e8770b7b8d7779cab00ee792ccf11b476dff93a
Zoltan Czako <czzo@erni.ro>
2019-10-23 10:12:15 +0300

    Setup PyPi package

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
   author = 'Czako Zoltan',                   # Type in your name
   author_email = 'czakozoltan08@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/CzakoZoltan08/AutomaticAI',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/CzakoZoltan08/AutomaticAI/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['AI', 'Arificial Intelligence', 'Particle Swarm Optimization', 'Simulated Annealing', 'Automatic Algorithm Selection', 'Automatic parameter optimization'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'sklearn',
+          'numpy',
+		  'scipy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
